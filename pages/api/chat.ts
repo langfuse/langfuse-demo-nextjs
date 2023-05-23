d54cffb7f05d211d9c71115d6ad6d7dbe8cf71fa
Max Deichmann <m.deichmann@tum.de>
2023-05-23 19:56:10 +0200

    wip

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,14 +1,7 @@
 import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 
-import { ChatBody, Message } from '@/types/chat';
-
-// @ts-expect-error
-import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
-
-import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
-import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
-import { LangfuseClient, LangfuseEnvironment } from '@finto-fern/api-client';
+import { ChatBody } from '@/types/chat';
 import { NextApiRequest } from 'next';
 
 
@@ -17,9 +10,9 @@ const handler = async (req: NextApiRequest): Promise<Response> => {
     
     const { model, messages, key, prompt, temperature } = (req.body) as ChatBody;
     
-    const client = new LangfuseClient({
-      environment: LangfuseEnvironment.Local
-    });
+    // const client = new LangfuseClient({
+    //   environment: LangfuseEnvironment.Local
+    // });
 
     // const trace = await client.trace.create({
     //   name: 'chat-completion',
