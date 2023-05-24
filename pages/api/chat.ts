0b03a697851595e74482f17590dd3c97dc13376c
Marc Klingen <git@marcklingen.com>
2023-05-24 20:59:56 +0200

    Comment langfuse

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,24 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
 import { DEFAULT_SYSTEM_PROMPT, DEFAULT_TEMPERATURE } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 
 import { ChatBody } from '@/types/chat';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { LangfuseClient, LangfuseEnvironment } from '@finto-fern/api-client';
+
+// import { LangfuseClient, LangfuseEnvironment } from '@finto-fern/api-client';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    
     const { model, messages, key, prompt, temperature } = req.body as ChatBody;
 
-    const client = new LangfuseClient({
-      environment: 'http://localhost:3030'
-    });
+    // const client = new LangfuseClient({
+    //   environment: 'http://localhost:3030'
+    // });
 
-    const trace = await client.trace.create({
-      name: 'chat-completion',
-      attributes: { env: LangfuseEnvironment.Local },
-      status: 'executing'
-    })
+    // const trace = await client.trace.create({
+    //   name: 'chat-completion',
+    //   attributes: { env: LangfuseEnvironment.Local },
+    //   status: 'executing'
+    // })
 
     let promptToSend = prompt;
     if (!promptToSend) {
@@ -34,30 +35,36 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return {
         role: message.role,
         content: message.content,
-      }
-    })
+      };
+    });
 
-    const llmCall = await client.span.createLlmCall({
-      traceId: trace.id,
-      startTime: new Date(),
-      name: 'chat-completion',
-      attributes: {
-        model: {
-          modelId: model.id,
-          modelName: model.name,
-        },
-        prompt: JSON.stringify([
-          {
-            role: 'system',
-            content: promptToSend,
-          },
-          ...messagesToSend,
-        ],),
-      },
-    })
+    // const llmCall = await client.span.createLlmCall({
+    //   traceId: trace.id,
+    //   startTime: new Date(),
+    //   name: 'chat-completion',
+    //   attributes: {
+    //     model: {
+    //       modelId: model.id,
+    //       modelName: model.name,
+    //     },
+    //     prompt: JSON.stringify([
+    //       {
+    //         role: 'system',
+    //         content: promptToSend,
+    //       },
+    //       ...messagesToSend,
+    //     ],),
+    //   },
+    // })
 
-    const stream = await OpenAIStream(model, promptToSend, temperatureToUse, key, messagesToSend);  
-    
+    const stream = await OpenAIStream(
+      model,
+      promptToSend,
+      temperatureToUse,
+      key,
+      messagesToSend,
+    );
+
     const reader = stream.getReader();
 
     let completeResp = '';
@@ -68,21 +75,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       completeResp += new TextDecoder().decode(value);
     }
 
-    await client.span.updateLlmCall({
-      spanId: llmCall.id,
-      endTime: new Date(),
-      attributes: {
-        completion: completeResp,
-      },
+    // await client.span.updateLlmCall({
+    //   spanId: llmCall.id,
+    //   endTime: new Date(),
+    //   attributes: {
+    //     completion: completeResp,
+    //   },
+    // });
+
+    // await client.trace.update({
+    //   id: trace.id,
+    //   status: 'success'
+    // })
+
+    res.status(200).json({
+      response: completeResp,
+      // traceId: trace.id
     });
-
-    await client.trace.update({
-      id: trace.id,
-      status: 'success'
-    })
-
-    res.status(200).json({response: completeResp, traceId: trace.id});
-
   } catch (error) {
     console.error(error);
     if (error instanceof OpenAIError) {
