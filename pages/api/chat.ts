1b1501a3163fcf5a69726de03d40c101dfbdcd0f
Max Deichmann <m.deichmann@tum.de>
2023-06-22 10:25:43 +0200

    execute on edge

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -5,19 +5,23 @@ import { OpenAIError } from '@/utils/server';
 
 import { ChatBody } from '@/types/chat';
 import { Configuration, OpenAIApi } from 'openai';
-// import { LangfuseClient } from '@finto-fern/langfuse-node';
+import { LangfuseClient } from '@finto-fern/langfuse-node';
 import { isAxiosError } from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
 
+export const config = {
+  runtime: 'edge', // this is a pre-requisite
+};
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextRequest, res: NextResponse) => {
   try {
-    const { model, messages, key, prompt, temperature } = req.body as ChatBody;
-
-    // const client = new LangfuseClient({
-    //   environment: 'https://cloud.langfuse.com',
-    //   username: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY!, // 'pk-lf-...43d',
-    //   password: process.env.SECRET_KEY!, //'sk-lf-...959'
-    // });
+    const { model, messages, key, prompt, temperature } = await req.json() as ChatBody;
+    console.log(model, messages)
+    const client = new LangfuseClient({
+      environment: 'https://cloud.langfuse.com',
+      username: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY!, // 'pk-lf-...43d',
+      password: process.env.SECRET_KEY!, //'sk-lf-...959'
+    });
 
     let systemPrompt = prompt;
     if (!systemPrompt) {
@@ -35,27 +39,29 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         content: message.content,
       };
     });
+    console.log('befiretrace')
 
-    // const trace = await client.trace.create({
-    //   name: 'chat',
-    //   metadata: {
-    //     userId: 'user__935d7d1d-8625-4ef4-8651-544613e7bd22',
-    //   },
-    // });
+    const trace = await client.trace.create({
+      name: 'chat',
+      metadata: {
+        userId: 'user__935d7d1d-8625-4ef4-8651-544613e7bd22',
+      },
+    });
+    console.log('after befiretrace')
     
     const startTimeUserSearch = new Date();
 
     //const dbUser = await client.users.get({ username: 'user__935d7d1d-8625-4ef4-8651-544613e7bd22' });
 
-    // const userObservation = await client.span.create({
-    //   startTime: startTimeUserSearch,
-    //   endTime: new Date(),
-    //   name: 'user-search',
-    //   metadata: {
-    //     userId: 'user__935d7d1d-8625-4ef4-8651-544613e7bd22',
-    //   },
-    //   traceId: trace.id
-    // });
+    const userObservation = await client.span.create({
+      startTime: startTimeUserSearch,
+      endTime: new Date(),
+      name: 'user-search',
+      metadata: {
+        userId: 'user__935d7d1d-8625-4ef4-8651-544613e7bd22',
+      },
+      traceId: trace.id
+    });
 
     const configuration = new Configuration({
       apiKey: key,
@@ -105,22 +111,22 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     //     userId: "user__935d7d1d-8625-4ef4-8651-544613e7bd22"
     //   },
     // })
-
-    res.status(200).json({
+    
+    return NextResponse.json({
       response: chatCompletion.data.choices[0].message?.content,
       // traceId: generation.traceId
     });
   } catch (error) {
 
     isAxiosError(error)
-      ? console.log('error sending error response: ', error.message, error.response?.data)
-      : console.log('error sending error response: ', error); 
+      ? console.log('axios error: ', error.message, error.response?.data)
+      : console.log('error: ', error); 
 
 
     if (error instanceof OpenAIError) {
-      return new Response('Error', { status: 500, statusText: error.message }); 
+      return  NextResponse.json('Error', { status: 500, statusText: error.message }); 
     } else {
-      return new Response('Error', { status: 500 });
+      return NextResponse.json('Error', { status: 500 });
     }
   }
 };
